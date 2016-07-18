52481122b98d2f75a32054ecf63e6187b053151b
cadon <cadaei@web.de>
2016-07-18 15:13:08 +0200

    fix for Giganotosaurus-Incubation-time

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.20.2.0")]
+[assembly: AssemblyFileVersion("0.20.3.0")]
