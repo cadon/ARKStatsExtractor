54f47eadf039e0c72ff9d06c7316cd48164e173d
fenglisu <l765335321@163.com>
2017-05-25 22:27:43 +0800

    Update - 部落面板汉化内容

--- ARKBreedingStats/TribesControl.Designer.cs
+++ ARKBreedingStats/TribesControl.Designer.cs
@@ -84,7 +84,7 @@
             this.listViewPlayer.Location = new System.Drawing.Point(280, 3);
             this.listViewPlayer.Name = "listViewPlayer";
             this.tableLayoutPanel1.SetRowSpan(this.listViewPlayer, 2);
-            this.listViewPlayer.Size = new System.Drawing.Size(462, 503);
+            this.listViewPlayer.Size = new System.Drawing.Size(462, 464);
             this.listViewPlayer.TabIndex = 1;
             this.listViewPlayer.UseCompatibleStateImageBehavior = false;
             this.listViewPlayer.View = System.Windows.Forms.View.Details;
@@ -94,27 +94,27 @@
             // 
             // columnHeaderPlayer
             // 
-            this.columnHeaderPlayer.Text = "Player";
+            this.columnHeaderPlayer.Text = "玩家";
             this.columnHeaderPlayer.Width = 85;
             // 
             // columnHeaderLvl
             // 
-            this.columnHeaderLvl.Text = "Level";
+            this.columnHeaderLvl.Text = "等级";
             this.columnHeaderLvl.Width = 38;
             // 
             // columnHeaderTribe
             // 
-            this.columnHeaderTribe.Text = "Tribe";
+            this.columnHeaderTribe.Text = "部落";
             this.columnHeaderTribe.Width = 77;
             // 
             // columnHeaderRel
             // 
-            this.columnHeaderRel.Text = "Relation";
+            this.columnHeaderRel.Text = "关系";
             this.columnHeaderRel.Width = 51;
             // 
             // columnHeaderNotes
             // 
-            this.columnHeaderNotes.Text = "Notes";
+            this.columnHeaderNotes.Text = "备注";
             this.columnHeaderNotes.Width = 198;
             // 
             // tableLayoutPanel1
@@ -129,10 +129,10 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 214F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 198F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(745, 509);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(745, 470);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // panelSettings
@@ -142,7 +142,7 @@
             this.panelSettings.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelSettings.Location = new System.Drawing.Point(3, 3);
             this.panelSettings.Name = "panelSettings";
-            this.panelSettings.Size = new System.Drawing.Size(271, 208);
+            this.panelSettings.Size = new System.Drawing.Size(271, 192);
             this.panelSettings.TabIndex = 11;
             // 
             // panelPlayerSettings
@@ -160,7 +160,7 @@
             this.panelPlayerSettings.Enabled = false;
             this.panelPlayerSettings.Location = new System.Drawing.Point(0, 0);
             this.panelPlayerSettings.Name = "panelPlayerSettings";
-            this.panelPlayerSettings.Size = new System.Drawing.Size(271, 208);
+            this.panelPlayerSettings.Size = new System.Drawing.Size(271, 192);
             this.panelPlayerSettings.TabIndex = 8;
             // 
             // label8
@@ -168,84 +168,84 @@
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(3, 5);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(265, 19);
+            this.label8.Size = new System.Drawing.Size(265, 18);
             this.label8.TabIndex = 11;
-            this.label8.Text = "Player";
+            this.label8.Text = "玩家";
             this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 34);
+            this.label1.Location = new System.Drawing.Point(3, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Playername";
+            this.label1.Text = "玩家名字";
             // 
             // textBoxPlayerTribe
             // 
             this.textBoxPlayerTribe.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.textBoxPlayerTribe.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.textBoxPlayerTribe.Location = new System.Drawing.Point(66, 82);
+            this.textBoxPlayerTribe.Location = new System.Drawing.Point(66, 76);
             this.textBoxPlayerTribe.Name = "textBoxPlayerTribe";
-            this.textBoxPlayerTribe.Size = new System.Drawing.Size(193, 20);
+            this.textBoxPlayerTribe.Size = new System.Drawing.Size(193, 21);
             this.textBoxPlayerTribe.TabIndex = 5;
             this.textBoxPlayerTribe.TextChanged += new System.EventHandler(this.textBoxPlayerTribe_TextChanged);
             // 
             // textBoxPlayerName
             // 
-            this.textBoxPlayerName.Location = new System.Drawing.Point(66, 30);
+            this.textBoxPlayerName.Location = new System.Drawing.Point(66, 28);
             this.textBoxPlayerName.Name = "textBoxPlayerName";
-            this.textBoxPlayerName.Size = new System.Drawing.Size(193, 20);
+            this.textBoxPlayerName.Size = new System.Drawing.Size(193, 21);
             this.textBoxPlayerName.TabIndex = 1;
             this.textBoxPlayerName.TextChanged += new System.EventHandler(this.textBoxPlayerName_TextChanged);
             // 
             // textBoxPlayerNotes
             // 
-            this.textBoxPlayerNotes.Location = new System.Drawing.Point(66, 108);
+            this.textBoxPlayerNotes.Location = new System.Drawing.Point(66, 100);
             this.textBoxPlayerNotes.Multiline = true;
             this.textBoxPlayerNotes.Name = "textBoxPlayerNotes";
-            this.textBoxPlayerNotes.Size = new System.Drawing.Size(193, 91);
+            this.textBoxPlayerNotes.Size = new System.Drawing.Size(193, 84);
             this.textBoxPlayerNotes.TabIndex = 7;
             this.textBoxPlayerNotes.TextChanged += new System.EventHandler(this.textBoxPlayerNotes_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 86);
+            this.label2.Location = new System.Drawing.Point(3, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Tribe";
+            this.label2.Text = "部落";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 112);
+            this.label4.Location = new System.Drawing.Point(3, 100);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Notes";
+            this.label4.Text = "备注";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 59);
+            this.label3.Location = new System.Drawing.Point(3, 56);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Level";
+            this.label3.Text = "等级";
             // 
             // numericUpDownLevel
             // 
-            this.numericUpDownLevel.Location = new System.Drawing.Point(66, 56);
+            this.numericUpDownLevel.Location = new System.Drawing.Point(66, 52);
             this.numericUpDownLevel.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.numericUpDownLevel.Name = "numericUpDownLevel";
-            this.numericUpDownLevel.Size = new System.Drawing.Size(77, 20);
+            this.numericUpDownLevel.Size = new System.Drawing.Size(77, 21);
             this.numericUpDownLevel.TabIndex = 3;
             this.numericUpDownLevel.ValueChanged += new System.EventHandler(this.numericUpDownLevel_ValueChanged);
             // 
@@ -261,7 +261,7 @@
             this.panelTribeSettings.Enabled = false;
             this.panelTribeSettings.Location = new System.Drawing.Point(0, 0);
             this.panelTribeSettings.Name = "panelTribeSettings";
-            this.panelTribeSettings.Size = new System.Drawing.Size(271, 208);
+            this.panelTribeSettings.Size = new System.Drawing.Size(271, 192);
             this.panelTribeSettings.TabIndex = 9;
             // 
             // label7
@@ -269,7 +269,7 @@
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(3, 5);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(265, 19);
+            this.label7.Size = new System.Drawing.Size(265, 18);
             this.label7.TabIndex = 10;
             this.label7.Text = "Tribe";
             this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -280,9 +280,9 @@
             this.groupBox1.Controls.Add(this.radioButtonHostile);
             this.groupBox1.Controls.Add(this.radioButtonNeutral);
             this.groupBox1.Controls.Add(this.radioButtonAllied);
-            this.groupBox1.Location = new System.Drawing.Point(66, 53);
+            this.groupBox1.Location = new System.Drawing.Point(66, 49);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(151, 66);
+            this.groupBox1.Size = new System.Drawing.Size(151, 61);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Relation";
@@ -290,9 +290,9 @@
             // radioButtonFriendly
             // 
             this.radioButtonFriendly.AutoSize = true;
-            this.radioButtonFriendly.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonFriendly.Location = new System.Drawing.Point(6, 39);
             this.radioButtonFriendly.Name = "radioButtonFriendly";
-            this.radioButtonFriendly.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonFriendly.Size = new System.Drawing.Size(71, 16);
             this.radioButtonFriendly.TabIndex = 1;
             this.radioButtonFriendly.TabStop = true;
             this.radioButtonFriendly.Text = "Friendly";
@@ -302,9 +302,9 @@
             // radioButtonHostile
             // 
             this.radioButtonHostile.AutoSize = true;
-            this.radioButtonHostile.Location = new System.Drawing.Point(86, 42);
+            this.radioButtonHostile.Location = new System.Drawing.Point(86, 39);
             this.radioButtonHostile.Name = "radioButtonHostile";
-            this.radioButtonHostile.Size = new System.Drawing.Size(57, 17);
+            this.radioButtonHostile.Size = new System.Drawing.Size(65, 16);
             this.radioButtonHostile.TabIndex = 3;
             this.radioButtonHostile.TabStop = true;
             this.radioButtonHostile.Text = "Hostile";
@@ -314,9 +314,9 @@
             // radioButtonNeutral
             // 
             this.radioButtonNeutral.AutoSize = true;
-            this.radioButtonNeutral.Location = new System.Drawing.Point(86, 19);
+            this.radioButtonNeutral.Location = new System.Drawing.Point(86, 18);
             this.radioButtonNeutral.Name = "radioButtonNeutral";
-            this.radioButtonNeutral.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonNeutral.Size = new System.Drawing.Size(65, 16);
             this.radioButtonNeutral.TabIndex = 2;
             this.radioButtonNeutral.TabStop = true;
             this.radioButtonNeutral.Text = "Neutral";
@@ -326,9 +326,9 @@
             // radioButtonAllied
             // 
             this.radioButtonAllied.AutoSize = true;
-            this.radioButtonAllied.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonAllied.Location = new System.Drawing.Point(6, 18);
             this.radioButtonAllied.Name = "radioButtonAllied";
-            this.radioButtonAllied.Size = new System.Drawing.Size(50, 17);
+            this.radioButtonAllied.Size = new System.Drawing.Size(59, 16);
             this.radioButtonAllied.TabIndex = 0;
             this.radioButtonAllied.TabStop = true;
             this.radioButtonAllied.Text = "Allied";
@@ -337,36 +337,36 @@
             // 
             // textBoxTribeNotes
             // 
-            this.textBoxTribeNotes.Location = new System.Drawing.Point(66, 125);
+            this.textBoxTribeNotes.Location = new System.Drawing.Point(66, 115);
             this.textBoxTribeNotes.Multiline = true;
             this.textBoxTribeNotes.Name = "textBoxTribeNotes";
-            this.textBoxTribeNotes.Size = new System.Drawing.Size(188, 72);
+            this.textBoxTribeNotes.Size = new System.Drawing.Size(188, 67);
             this.textBoxTribeNotes.TabIndex = 9;
             this.textBoxTribeNotes.TextChanged += new System.EventHandler(this.textBoxTribeNotes_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 128);
+            this.label6.Location = new System.Drawing.Point(3, 118);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(35, 12);
             this.label6.TabIndex = 8;
             this.label6.Text = "Notes";
             // 
             // textBoxTribeName
             // 
-            this.textBoxTribeName.Location = new System.Drawing.Point(66, 27);
+            this.textBoxTribeName.Location = new System.Drawing.Point(66, 25);
             this.textBoxTribeName.Name = "textBoxTribeName";
-            this.textBoxTribeName.Size = new System.Drawing.Size(188, 20);
+            this.textBoxTribeName.Size = new System.Drawing.Size(188, 21);
             this.textBoxTribeName.TabIndex = 6;
             this.textBoxTribeName.TextChanged += new System.EventHandler(this.textBoxTribeName_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 30);
+            this.label5.Location = new System.Drawing.Point(3, 28);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.Size = new System.Drawing.Size(59, 12);
             this.label5.TabIndex = 5;
             this.label5.Text = "Tribename";
             // 
@@ -374,12 +374,12 @@
             // 
             this.groupBox2.Controls.Add(this.listViewTribes);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(3, 217);
+            this.groupBox2.Location = new System.Drawing.Point(3, 201);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(271, 289);
+            this.groupBox2.Size = new System.Drawing.Size(271, 266);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Tribes";
+            this.groupBox2.Text = "部落";
             // 
             // listViewTribes
             // 
@@ -389,10 +389,10 @@
             this.listViewTribes.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listViewTribes.FullRowSelect = true;
             this.listViewTribes.HideSelection = false;
-            this.listViewTribes.Location = new System.Drawing.Point(3, 16);
+            this.listViewTribes.Location = new System.Drawing.Point(3, 17);
             this.listViewTribes.MultiSelect = false;
             this.listViewTribes.Name = "listViewTribes";
-            this.listViewTribes.Size = new System.Drawing.Size(265, 270);
+            this.listViewTribes.Size = new System.Drawing.Size(265, 246);
             this.listViewTribes.TabIndex = 0;
             this.listViewTribes.UseCompatibleStateImageBehavior = false;
             this.listViewTribes.View = System.Windows.Forms.View.Details;
@@ -402,21 +402,21 @@
             // 
             // columnHeaderName
             // 
-            this.columnHeaderName.Text = "Name";
+            this.columnHeaderName.Text = "名称";
             this.columnHeaderName.Width = 199;
             // 
             // columnHeaderRelation
             // 
-            this.columnHeaderRelation.Text = "Relation";
+            this.columnHeaderRelation.Text = "关系";
             this.columnHeaderRelation.Width = 54;
             // 
             // TribesControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "TribesControl";
-            this.Size = new System.Drawing.Size(745, 509);
+            this.Size = new System.Drawing.Size(745, 470);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panelSettings.ResumeLayout(false);
             this.panelPlayerSettings.ResumeLayout(false);
