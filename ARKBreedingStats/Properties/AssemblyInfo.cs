20c41d4bdd879196b7d76e5d75639b663c14e132
cadaei <cadaei@web.de>
2017-05-30 22:21:48 +0200

    updated ver

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.23.3")]
+[assembly: AssemblyFileVersion("0.23.4")]
