151e9bb27fde2e9f7e6ccad9b0bfc89ace176227
cadaei <cadaei@web.de>
2017-08-20 12:39:11 +0200

    added names for color-regions of thorny dragon.

--- ARKBreedingStats/Form1.Designer.cs
+++ ARKBreedingStats/Form1.Designer.cs
@@ -1475,7 +1475,7 @@
             // 
             // creatureInfoInputTester
             // 
-            this.creatureInfoInputTester.Cooldown = new System.DateTime(2017, 8, 16, 22, 26, 38, 926);
+            this.creatureInfoInputTester.Cooldown = new System.DateTime(2017, 8, 20, 12, 7, 4, 426);
             this.creatureInfoInputTester.CreatureName = "";
             this.creatureInfoInputTester.CreatureNote = "";
             this.creatureInfoInputTester.CreatureOwner = "";
@@ -1484,7 +1484,7 @@
             this.creatureInfoInputTester.CreatureTribe = "";
             this.creatureInfoInputTester.domesticatedAt = new System.DateTime(2016, 7, 5, 13, 11, 41, 997);
             this.creatureInfoInputTester.father = null;
-            this.creatureInfoInputTester.Grown = new System.DateTime(2017, 8, 16, 22, 26, 38, 927);
+            this.creatureInfoInputTester.Grown = new System.DateTime(2017, 8, 20, 12, 7, 4, 427);
             this.creatureInfoInputTester.Location = new System.Drawing.Point(321, 184);
             this.creatureInfoInputTester.mother = null;
             this.creatureInfoInputTester.MutationCounter = 0;
@@ -1816,7 +1816,7 @@
             // 
             // creatureInfoInputExtractor
             // 
-            this.creatureInfoInputExtractor.Cooldown = new System.DateTime(2017, 8, 16, 22, 26, 38, 974);
+            this.creatureInfoInputExtractor.Cooldown = new System.DateTime(2017, 8, 20, 12, 7, 4, 457);
             this.creatureInfoInputExtractor.CreatureName = "";
             this.creatureInfoInputExtractor.CreatureNote = "";
             this.creatureInfoInputExtractor.CreatureOwner = "";
@@ -1825,7 +1825,7 @@
             this.creatureInfoInputExtractor.CreatureTribe = "";
             this.creatureInfoInputExtractor.domesticatedAt = new System.DateTime(2016, 7, 5, 13, 12, 15, 968);
             this.creatureInfoInputExtractor.father = null;
-            this.creatureInfoInputExtractor.Grown = new System.DateTime(2017, 8, 16, 22, 26, 38, 975);
+            this.creatureInfoInputExtractor.Grown = new System.DateTime(2017, 8, 20, 12, 7, 4, 457);
             this.creatureInfoInputExtractor.Location = new System.Drawing.Point(321, 184);
             this.creatureInfoInputExtractor.mother = null;
             this.creatureInfoInputExtractor.MutationCounter = 0;
