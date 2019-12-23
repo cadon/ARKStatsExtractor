1a27299e24051b6fb766d726ee787ccdd2d7a28d
cadaei <cadaei@web.de>
2019-12-23 21:41:34 +0100

    cleanup

--- ARKBreedingStats/values/Values.cs
+++ ARKBreedingStats/values/Values.cs
@@ -8,11 +8,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ARKBreedingStats.values
 {
