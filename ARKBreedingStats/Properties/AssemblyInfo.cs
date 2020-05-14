0c83b02212398b20a95e1822b7e0e8ac352b21b2
cadaei <cadaei@web.de>
2020-05-14 21:23:21 +0200

    ver

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.39.5.1")]
+[assembly: AssemblyFileVersion("0.39.6.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
