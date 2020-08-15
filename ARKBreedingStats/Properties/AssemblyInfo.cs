57fc25c385575d0ce790e9d11b44a4a9a08e70e1
cadaei <cadaei@web.de>
2020-08-15 19:14:38 +0200

    change to minor version update instead of patch

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.40.6.0")]
+[assembly: AssemblyFileVersion("0.41.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
