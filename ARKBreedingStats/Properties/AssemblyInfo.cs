e47656f6d9677de5098b7491c6444914513c89ff
cadon <cadon@users.noreply.github.com>
2021-01-18 20:46:49 +0100

    ver. values

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ARK Smart Breeding")]
-[assembly: AssemblyCopyright("Copyright © 2015 – 2019, made by cadon")]
+[assembly: AssemblyCopyright("Copyright © 2015 – 2021, made by cadon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.43.0.1")]
+[assembly: AssemblyFileVersion("0.43.0.2")]
 [assembly: NeutralResourcesLanguage("en")]
 
