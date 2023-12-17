720f22e79f6a98a83f0f48f6e6c8f20ca32dda66
cadon <cadon@users.noreply.github.com>
2023-12-17 20:54:07 +0100

    null fix

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.59.3.1")]
+[assembly: AssemblyFileVersion("0.59.3.2")]
 [assembly: NeutralResourcesLanguage("en")]
 
