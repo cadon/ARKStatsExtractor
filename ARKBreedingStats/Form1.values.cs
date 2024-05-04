b8d0a39736cba0513a42d54d1722d1867d8aa9c0
cadon <cadon@users.noreply.github.com>
2024-05-04 12:23:14 +0200

    improved info when importing ASA species in ASE mode

--- ARKBreedingStats/Form1.values.cs
+++ ARKBreedingStats/Form1.values.cs
@@ -61,6 +61,7 @@ namespace ARKBreedingStats
             bool alreadyLoadedModFilesWithoutNeededClassExist = alreadyLoadedModFilesWithoutNeededClass != null && alreadyLoadedModFilesWithoutNeededClass.Any();
 
             MessageBoxes.ShowMessageBox("Some of the creatures to be imported have an unknown species, most likely because a mod is used.\n"
+                + "If you play Ark: Survival Ascended, make sure you have set the mode to ASA in the settings.\n"
                 + "To import these creatures, this application needs additional information about these mods."
                 + (locallyAvailableModsExist ?
                     "\n\nThe value files for the following mods are already locally available and just need to be added to the library:\n\n- "
