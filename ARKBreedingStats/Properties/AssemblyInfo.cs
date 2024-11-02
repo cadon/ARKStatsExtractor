8292dff2b3a8611db33f49188b1a816b97af4d36
cadon <cadon@users.noreply.github.com>
2024-11-02 14:09:39 +0100

    ver

--- ARKBreedingStats/Properties/AssemblyInfo.cs
+++ ARKBreedingStats/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.63.1.3")]
+[assembly: AssemblyFileVersion("0.64.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
