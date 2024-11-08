1cd2550d697b61507a5fd67f74b2548148684ab6
cadon <cadon@users.noreply.github.com>
2024-11-08 21:31:10 +0100

    logging

---
+++ ARKBreedingStats/utils/Logging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ARKBreedingStats.utils
+{
+    internal class Logging
+    {
+        internal static void Log(string text, string logFileName = null)
+        {
+            if (string.IsNullOrEmpty(logFileName))
+                logFileName = "log.txt";
+            File.AppendAllText(logFileName, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}: {text}{Environment.NewLine}");
+        }
+    }
+}
