0f743a5acf031b7d1aa711b9b45e21e43f215c25
cadon <cadon@users.noreply.github.com>
2025-11-24 22:19:41 +0100

    breeding planner include cooldown by default

--- ARKBreedingStats/Properties/Settings.Designer.cs
+++ ARKBreedingStats/Properties/Settings.Designer.cs
@@ -654,7 +654,7 @@ namespace ARKBreedingStats.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IncludeCooldownsInBreedingPlan {
             get {
                 return ((bool)(this["IncludeCooldownsInBreedingPlan"]));
