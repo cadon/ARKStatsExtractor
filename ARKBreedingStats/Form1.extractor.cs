0b7de0a20bf8e123f2781b3b6902f184ac9d70c3
cadon <cadon@users.noreply.github.com>
2025-12-02 23:41:46 +0100

    fix: hide colored creature on extraction error

--- ARKBreedingStats/Form1.extractor.cs
+++ ARKBreedingStats/Form1.extractor.cs
@@ -682,6 +682,7 @@ namespace ARKBreedingStats
             groupBoxRadarChartExtractor.Visible = false;
             creatureAnalysis1.Visible = false;
             lbInfoYellowStats.Visible = false;
+            PbCreatureColorsExtractor.Visible = false;
             BtCopyIssueDumpToClipboard.Visible = true;
             string redInfoText = null;
             if (rbBredExtractor.Checked && numericUpDownImprintingBonusExtractor.Value > 0)
