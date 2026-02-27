e03569a2abe1604c37e83f6ca84fd98a5a3ceb98
cadon <cadon@users.noreply.github.com>
2026-02-27 21:33:29 +0100

    extraction fail help: speed level if bonded taming is set

--- ARKBreedingStats/Form1.extractor.cs
+++ ARKBreedingStats/Form1.extractor.cs
@@ -456,7 +456,7 @@ namespace ARKBreedingStats
                         possibleExtractionIssues |= IssueNotes.Issue.SinglePlayer;
                     }
                     // if the stat is speed, the allowSpeedLeveling could be set incorrectly. For bred creatures that setting affects if speed is changed by imprinting.
-                    if (s == Stats.SpeedMultiplier && rbBredExtractor.Checked && numericUpDownImprintingBonusExtractor.Value > 0)
+                    if (s == Stats.SpeedMultiplier && (rbBredExtractor.Checked || !RbBondedTaming0.Checked) && numericUpDownImprintingBonusExtractor.Value > 0)
                     {
                         possibleExtractionIssues |= IssueNotes.Issue.SpeedLevelingSetting;
                     }
