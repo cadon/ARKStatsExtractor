1a6620aa3b91edf92184ef113aa06d5cd66586aa
Olfi01 <flom@meyer-alpers.de>
2026-05-14 23:26:00 +0200

    Implement ASA savegame import

--- ARKBreedingStats/ImportSavegame.cs
+++ ARKBreedingStats/ImportSavegame.cs
@@ -13,6 +13,16 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Creature = ARKBreedingStats.Library.Creature;
+using SavegameCreature = AsaSavegameToolkit.Porcelain.Creature;
+using GameObjectExtensions = SavegameToolkitAdditions.GameObjectExtensions;
+using AsaSavegameToolkit.Plumbing.Utilities;
+#if DEBUG
+using AsaSavegameToolkit.Plumbing.Records;
+#else
+using AsaSavegameToolkit.Plumbing.Properties;
+#endif
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace ARKBreedingStats
 {
@@ -58,10 +68,43 @@ namespace ARKBreedingStats
 
         private static async Task<Creature[]> ImportCollectionFromAsaSavegame(CreatureCollection creatureCollection, string filename, string serverName)
         {
+            var ignoreClasses = Values.V.IgnoreSpeciesClassesOnImport;
             var importUnclaimedBabies = Properties.Settings.Default.SaveFileImportUnclaimedBabies;
             var saveImportCryo = Properties.Settings.Default.SaveImportCryo;
 
-            throw new NotImplementedException();
+            var save = AsaSaveGame.ReadFrom(filename, logger: NullLogger.Instance);
+#if DEBUG
+            var gameTime = save.GameTime;
+#else
+            var gameTime = 0d; // wait for https://github.com/flintthatchwood/AsaSavegameToolkit/pull/3 to be merged and published
+#endif
+
+            var tamedCreatureObjects = save.TamedCreatures.Values.Where(o => (importUnclaimedBabies || !o.Record.IsUnclaimedBaby()) && !ignoreClasses.Contains(o.ClassName.Split('.').Last()));
+            if (saveImportCryo)
+            {
+                tamedCreatureObjects = tamedCreatureObjects.Concat(save.CryopoddedCreatures.Values.Where(o => !ignoreClasses.Contains(o.ClassName.Split('.').Last())));
+            }
+
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.ImportTribeNameFilter))
+            {
+                string[] filters = Properties.Settings.Default.ImportTribeNameFilter.Split(',')
+                        .Select(s => s.Trim())
+                        .Where(s => !string.IsNullOrEmpty(s))
+                        .ToArray();
+
+                if (filters.Any())
+                {
+                    tamedCreatureObjects = tamedCreatureObjects.Where(o =>
+                    {
+                        string tribeName = o.TribeName ?? string.Empty;
+                        return filters.Any(filter => tribeName.Contains(filter));
+                    });
+                }
+            }
+
+            ImportSavegame importSavegame = new ImportSavegame(Convert.ToSingle(gameTime));
+            int? wildLevelStep = creatureCollection.getWildLevelStep();
+            return tamedCreatureObjects.Select(o => importSavegame.ConvertCreature(o, wildLevelStep)).Where(c => c != null).ToArray();
         }
 
         private static async Task<Creature[]> ImportCollectionFromAseSavegame(CreatureCollection creatureCollection, string filename, string serverName)
@@ -263,6 +306,147 @@ namespace ARKBreedingStats
             }
 
             bool isDead = creatureObject.GetPropertyValue<bool>("bIsDead");
+            if (isDead)
+            {
+                creature.Status = CreatureStatus.Dead; // dead is always dead
+            }
+
+            if (creatureObject.IsInCryo)
+                creature.Status = CreatureStatus.Cryopod;
+
+            creature.RecalculateCreatureValues(levelStep);
+
+            return creature;
+        }
+
+        private Creature ConvertCreature(SavegameCreature creatureObject, int? levelStep)
+        {
+            if (!Values.V.TryGetSpeciesByBlueprint(creatureObject.ClassName, out Species species))
+            {
+                // species is unknown, creature cannot be imported.
+                // use name-field to temporarily save the unknown classString to display in a messageBox
+                return new Creature { name = creatureObject.ClassName };
+            }
+
+            string imprinterName = creatureObject.ImprinterName;
+            string owner = string.IsNullOrWhiteSpace(imprinterName) ? creatureObject.TamerString : imprinterName;
+
+            int[] wildLevels = Enumerable.Repeat(-1, Stats.StatsCount).ToArray(); // -1 is unknown
+            int[] tamedLevels = new int[Stats.StatsCount];
+            int[] mutatedLevels = new int[Stats.StatsCount];
+
+            for (int i = 0; i < Stats.StatsCount; i++)
+            {
+                wildLevels[i] = creatureObject.WildLevels[i] ?? 0;
+            }
+            wildLevels[Stats.Torpidity] = (creatureObject.BaseLevel ?? 1) - 1; // torpor
+
+            for (int i = 0; i < Stats.StatsCount; i++)
+            {
+                tamedLevels[i] = creatureObject.TamedLevelsApplied[i] ?? 0;
+                mutatedLevels[i] = creatureObject.MutationLevelsApplied[i] ?? 0;
+            }
+
+            if (!creatureObject.Record.Properties.TryGet("TamedIneffectivenessModifier", out float ti))
+            {
+                ti = creatureObject.Record.Properties.Get<float>("TameIneffectivenessModifier");
+            }
+            double te = 1f / (1 + ti);
+
+            Guid creatureGuid = creatureObject.Id;
+            long arkId = Utils.ConvertCreatureGuidToArkId(creatureGuid);
+            Creature creature = new Creature(species,
+                    creatureObject.TamedName, owner, creatureObject.TribeName,
+                    creatureObject.IsFemale ? Sex.Female : Sex.Male,
+                    wildLevels, tamedLevels, mutatedLevels, te,
+                    !string.IsNullOrWhiteSpace(creatureObject.ImprinterName),
+                    creatureObject.ImprintQuality,
+                    levelStep
+            )
+            {
+                imprinterName = creatureObject.ImprinterName,
+                guid = creatureGuid,
+                ArkId = arkId,
+                ArkIdImported = true,
+                ArkIdInGame = Utils.ConvertImportedArkIdToIngameVisualization(arkId),
+                domesticatedAt = DateTime.Now,
+                addedToLibrary = DateTime.Now,
+                mutationsMaternal = creatureObject.MutationsFemale,
+                mutationsPaternal = creatureObject.MutationsMale,
+                flags = (creatureObject.Record.Properties.Get<bool>("bNeutered") ? CreatureFlags.Neutered : CreatureFlags.None)
+                      | (creatureObject.Record.Properties.Get<bool>("MutagenApplied") ? CreatureFlags.MutagenApplied : CreatureFlags.None)
+            };
+
+            // If it's a baby and still growing, work out growingUntil
+            float babyAge = creatureObject.BabyAge ?? 1;
+            if (babyAge < 1)
+            {
+                double maturationDuration = species.breeding?.maturationTimeAdjusted ?? 0;
+                float bornSecondsAgo = (float)maturationDuration * babyAge;
+                if (bornSecondsAgo < maturationDuration - 120) // there seems to be a slight offset of one of these saved values, so don't display a creature as being in cooldown if it is about to leave it in the next 2 minutes
+                    creature.growingUntil = DateTime.Now.Add(TimeSpan.FromSeconds(maturationDuration - bornSecondsAgo));
+            }
+            else
+            {
+                double nextMatingPossible = creatureObject.Record.Properties.Get<double>("NextAllowedMatingTime");
+                if (_gameTime < nextMatingPossible)
+                {
+                    creature.cooldownUntil = DateTime.Now.Add(TimeSpan.FromSeconds(nextMatingPossible - _gameTime));
+                }
+            }
+
+            // Ancestor linking is done later after entire collection is formed - here we just set the guids
+#if DEBUG
+            IEnumerable<DinoAncestorsEntryRecord> femaleAncestors = creatureObject.Record.Properties.Get<object[]>("DinoAncestors")?.OfType<DinoAncestorsEntryRecord>();
+            DinoAncestorsEntryRecord femaleAncestor = femaleAncestors?.LastOrDefault();
+            if (femaleAncestor != null)
+            {
+                creature.motherGuid = Utils.ConvertArkIdToGuid(Utils.ConvertArkIdsToLongArkId(
+                        (int)femaleAncestor.FemaleId1,
+                        (int)femaleAncestor.FemaleId2));
+                creature.motherName = femaleAncestor.FemaleName;
+                creature.isBred = true;
+            }
+            IEnumerable<DinoAncestorsEntryRecord> maleAncestors = creatureObject.Record.Properties.Get<object[]>("DinoAncestors")?.OfType<DinoAncestorsEntryRecord>();
+            DinoAncestorsEntryRecord maleAncestor = maleAncestors?.LastOrDefault();
+            if (maleAncestor != null)
+            {
+                creature.fatherGuid = Utils.ConvertArkIdToGuid(Utils.ConvertArkIdsToLongArkId(
+                        (int)maleAncestor.MaleId1,
+                        (int)maleAncestor.MaleId2));
+                creature.fatherName = maleAncestor.MaleName;
+                creature.isBred = true;
+            }
+#else
+            IEnumerable<List<Property>> femaleAncestors = creatureObject.Record.Properties.Get<object[]>("DinoAncestors")?.OfType<List<Property>>(); // for now, this has to use the raw properties until https://github.com/flintthatchwood/AsaSavegameToolkit/pull/4 is merged and published.
+            List<Property> femaleAncestor = femaleAncestors?.LastOrDefault();
+            if (femaleAncestor != null)
+            {
+                creature.motherGuid = Utils.ConvertArkIdToGuid(Utils.ConvertArkIdsToLongArkId(
+                        (int)femaleAncestor.Get<uint>("FemaleDinoID1"),
+                        (int)femaleAncestor.Get<uint>("FemaleDinoID2")));
+                creature.motherName = femaleAncestor.Get<string>("FemaleName");
+                creature.isBred = true;
+            }
+            IEnumerable<List<Property>> maleAncestors = creatureObject.Record.Properties.Get<object[]>("DinoAncestorsMale")?.OfType<List<Property>>();
+            List<Property> maleAncestor = maleAncestors?.LastOrDefault();
+            if (maleAncestor != null)
+            {
+                creature.fatherGuid = Utils.ConvertArkIdToGuid(GameObjectExtensions.CreateDinoId(
+                        (int)maleAncestor.Get<uint>("MaleDinoID1"),
+                        (int)maleAncestor.Get<uint>("MaleDinoID2")));
+                creature.fatherName = maleAncestor.Get<string>("MaleName");
+                creature.isBred = true;
+            }
+#endif
+
+            creature.colors = new byte[Ark.ColorRegionCount];
+            for (int i = 0; i < 6; i++)
+            {
+                creature.colors[i] = creatureObject.ColorRegions[i] ?? 0;
+            }
+
+            bool isDead = creatureObject.Record.Properties.Get<bool>("bIsDead");
             if (isDead)
             {
                 creature.Status = CreatureStatus.Dead; // dead is always dead
